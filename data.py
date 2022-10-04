6ab0bb7da674236ed2ee9fba323f77cb6948848c
LeoZDong <leodong0315@gmail.com>
2022-10-04 14:56:27 -0700

    Enable training with one region of the image.

--- data.py
+++ data.py
@@ -14,10 +14,11 @@ def get_mgrid(side_length: int, dim: int = 2) -> Tensor:
     Args:
         side_length: Side length of the grid (i.e. number of points).
         dim: Dimension of the grid.
+    Returns:
+        (side_length, side_length, dim) A tensor of `dim`-dimensional coordinates.
     """
     tensors = tuple(dim * [torch.linspace(-1, 1, steps=side_length)])
     mgrid = torch.stack(torch.meshgrid(*tensors, indexing="ij"), dim=-1)
-    mgrid = mgrid.reshape(-1, dim)
     return mgrid
 
 
@@ -42,24 +43,77 @@ def get_bsds_tensor_square(side_length: int, split: str, id: str) -> Tensor:
 
 
 class ImageFitting(Dataset):
-    """Dataset that represents a single image for SIREN fitting."""
+    """Dataset that represents a single image for SIREN fitting. One (x, y) data point
+    represents one coordinate and its corresponding pixel value in the image.
+    """
 
-    def __init__(self, side_length, split, id):
+    def __init__(self, side_length: int, data_split: str, id: str, divide_side_n: int):
+        """Initialize an image fitting dataset.
+        Args:
+            side_length: Side length of image (i.e. number of pixels on each side).
+            data_split: Data split of the BSDS dataset.
+            id: ID of the single image to use.
+            divide_side_n: Evenly divide each side into n segments. This results in n^2
+                regions to fit sequentially if using continual learning.
+        """
         super().__init__()
-        self.pixels = get_bsds_tensor_square(side_length, split, id)
-        self.coords = get_mgrid(side_length, 2)
+        spatial_dim = 2
 
-        # Flatten spatial dimension for training
-        self.pixels = self.pixels.reshape(-1, 3)
+        # Load full image and its coordinates
+        self.full_pixels = get_bsds_tensor_square(side_length, data_split, id)
+        self.full_coords = get_mgrid(side_length, spatial_dim)
+
+        # Split the image and coordinates into regions
+        side_length_per_region = side_length // divide_side_n
+        if side_length_per_region * divide_side_n != side_length:
+            print(
+                f"Warning: divide_side_n={divide_side_n} does not divide side length {side_length}."
+                "Some parts of the image will not belong ot any divided region!"
+            )
+
+        self.pixels_regions = []
+        self.coords_regions = []
+        for i in range(divide_side_n):
+            for j in range(divide_side_n):
+                pixels_region = self.full_pixels[
+                    i * side_length_per_region : (i + 1) * side_length_per_region,
+                    j * side_length_per_region : (j + 1) * side_length_per_region,
+                    :,
+                ]
+                coords_region = self.full_coords[
+                    i * side_length_per_region : (i + 1) * side_length_per_region,
+                    j * side_length_per_region : (j + 1) * side_length_per_region,
+                    :,
+                ]
+                self.pixels_regions.append(pixels_region.reshape(-1, 3))
+                self.coords_regions.append(coords_region.reshape(-1, spatial_dim))
+
+        # Flatten spatial dimension to obtain a batch of coordinates for training
+        self.full_pixels = self.full_pixels.reshape(-1, 3)
+        self.full_coords = self.full_coords.reshape(-1, spatial_dim)
+
+        # Initialize current region to be the first region
+        self.set_cur_region(0)
+
+    def set_cur_region(self, region: int) -> None:
+        self.cur_region = region
+        self.pixels = self.pixels_regions[self.cur_region]
+        self.coords = self.coords_regions[self.cur_region]
+
+    @property
+    def num_regions(self) -> int:
+        return len(self.pixels_regions)
 
     def __len__(self):
-        return 1
+        # return len(self.pixels_regions)
+        return self.pixels.shape[0]
 
     def __getitem__(self, idx):
-        if idx > 0:
-            raise IndexError
+        # if idx > 0:
+        # raise IndexError
 
         return self.coords, self.pixels
+        # return self.coords[idx], self.pixels[idx]
 
 
 if __name__ == "__main__":
--- trainers/simple_trainer.py
+++ trainers/simple_trainer.py
@@ -1,8 +1,12 @@
+import math
 import os
+from typing import Tuple
 
 import torch
 from hydra.utils import instantiate
 from omegaconf import DictConfig
+from torch import Tensor
+from torch.utils import data
 from torch.utils.tensorboard import SummaryWriter
 
 
@@ -25,28 +29,106 @@ class SimpleTrainer:
         total_steps = self.cfg.trainer.total_steps
 
         # Prepare data
+        dataloader = data.DataLoader(
+            self.dataset, batch_size=len(self.dataset), shuffle=False
+        )
         model_input, ground_truth = next(iter(self.dataset))
         model_input, ground_truth = model_input.to(self.device), ground_truth.to(
             self.device
         )
 
-        for step in range(total_steps + 1):
+        step = 0
+        while step <= total_steps:
             model_output, coords = self.siren(model_input)
             loss = ((model_output - ground_truth) ** 2).mean()
 
-            # Logging
+            # Logging training loss
             if step % self.cfg.trainer.summary_every_steps == 0:
                 self._tb_writer.add_scalar(
                     tag=f"train/loss",
                     scalar_value=loss.item(),
                     global_step=step,
                 )
-                img_out = (
-                    model_output.cpu().view(256, 256, -1).detach().permute(2, 0, 1)
+                print(f"step={step}, train_loss={round(loss.item(), 5)}")
+
+            # Logging evaluation loss and visualization
+            if step % self.cfg.trainer.eval_every_steps == 0:
+                eval_loss, full_img_out, full_ground_truth = self.eval(full_coords=True)
+                _, region_img_out, region_ground_truth = self.eval(full_coords=False)
+
+                self._tb_writer.add_scalar(
+                    tag="eval/loss",
+                    scalar_value=eval_loss,
+                    global_step=step,
                 )
-                self._tb_writer.add_image("train", img_out, global_step=step)
-                print(f"step={step}, loss={round(loss.item(), 5)}")
+
+                # Log model output image on the full coordinates
+                self._tb_writer.add_image(
+                    "full/eval_out_full", full_img_out, global_step=step
+                )
+                self._tb_writer.add_image(
+                    "full/gt_full", full_ground_truth, global_step=step
+                )
+
+                # Log model output image on the current region
+                region_id = self.dataset.cur_region
+                self._tb_writer.add_image(
+                    f"region/eval_out_region{region_id}",
+                    region_img_out,
+                    global_step=step,
+                )
+                self._tb_writer.add_image(
+                    f"region/gt_region{region_id}",
+                    region_ground_truth,
+                    global_step=step,
+                )
+
+                # Print eval stats
+                print(f"step={step}, eval_loss={round(eval_loss, 5)}")
 
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+            step += 1
+
+    @torch.no_grad()
+    def eval(self, full_coords: bool) -> Tuple[float, Tensor]:
+        """Evaluate the current `self.siren` model.
+        Args:
+            full_coords: Whether to evaluate on the entire coordinate grid or the
+                current coordinate region.
+        Returns:
+            eval_loss: Evaluation loss on the entire input image.
+            img_out (side_length, side_length, 3): Model output as float RGB tensor for
+                visualization.
+            ground_truth (side_length, side_length, 3): Ground truth image for comparison.
+        """
+        self.siren.eval()
+
+        if full_coords:
+            model_input, ground_truth = (
+                self.dataset.full_coords,
+                self.dataset.full_pixels,
+            )
+        else:
+            model_input, ground_truth = self.dataset.coords, self.dataset.pixels
+
+        model_output = self.siren(model_input)[0]
+        side_length = int(math.sqrt(model_output.shape[0]))
+
+        eval_loss = ((model_output - ground_truth) ** 2).mean().item()
+
+        # Recover spatial dimension for visualization
+        img_out = (
+            model_output.cpu()
+            .view(side_length, side_length, -1)
+            .detach()
+            .permute(2, 0, 1)
+        )
+
+        # Clamp image in [0, 1] for visualization
+        img_out = torch.clip(img_out, 0, 1)
+
+        self.siren.train()
+
+        return eval_loss, img_out, ground_truth
