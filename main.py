7ab755c1949206120ae3429eaf65907d45093b5d
LeoZDong <leodong0315@gmail.com>
2023-01-16 22:16:48 -0800

    Add random seed.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import random
+
 import hydra
+import numpy as np
+import torch
 from hydra.utils import instantiate
 from omegaconf import DictConfig
-
-import trainers.simple_trainer
 
 
 @hydra.main(config_path="conf", config_name="main", version_base="1.2")
 def run(cfg: DictConfig):
+    # Set seed
+    random.seed(cfg.seed)
+    np.random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+
     trainer = instantiate(cfg.trainer, cfg=cfg)
     return trainer.train()
 
