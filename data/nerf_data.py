9e54f76802f25f3e5b22ed9ae1ab9eb92abda97c
LeoZDong <leodong0315@gmail.com>
2023-04-04 14:39:37 -0700

    Update nerf data to use test split for video.

--- data/nerf_data.py
+++ data/nerf_data.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import cv2
 import hydra
@@ -110,8 +110,11 @@ class NeRFSyntheticDataset(Dataset):
     def __init__(
         self,
         path: str,
-        split: str,
+        split: Union[str, List],
+        img_w: int,
+        img_h: int,
         downsample: float,
+        use_test_split_for_video: bool,
         region_simulator: RegionSimulator,
     ) -> None:
         super().__init__()
@@ -119,26 +122,54 @@ class NeRFSyntheticDataset(Dataset):
             self.path = os.path.join(hydra.utils.get_original_cwd(), path)
         except:
             self.path = path
-        self.split = split
         self.downsample = downsample
         self.region_simulator = region_simulator
 
         # Fixed values for NeRF Synthetic dataset
-        self.img_w = int(800 * self.downsample)
-        self.img_h = int(800 * self.downsample)
+        self.img_w = int(img_w * self.downsample)
+        self.img_h = int(img_h * self.downsample)
 
         # Load data and metadata
-        self.read_intrinsics()
-        self.read_data(split)
+        # TODO: These do not need to be instance variables
+        self.K, self.directions = self.read_intrinsics()
+
+        if isinstance(split, List):
+            self.rays_o = []
+            self.rays_d = []
+            self.pixels = []
+            self.poses = []
+            for s in split:
+                rays_o, rays_d, pixels, poses = self.read_data(s, self.directions)
+                self.rays_o.append(rays_o)
+                self.rays_d.append(rays_d)
+                self.pixels.append(pixels)
+                self.poses.append(poses)
+            self.rays_o = torch.concat(self.rays_o)
+            self.rays_d = torch.concat(self.rays_d)
+            self.pixels = torch.concat(self.pixels)
+            self.poses = torch.concat(self.poses)
+        else:
+            self.rays_o, self.rays_d, self.pixels, self.poses = self.read_data(
+                split, self.directions
+            )
 
         # Generate a set of poses for video generation
-        self.generate_video_rays()
-        # Number of frames / poses for generating the test time video
-        num_frames_for_video = self.rays_o_video.shape[0]
-        self._video_inputs = [
-            {"rays_o": self.rays_o_video[i], "rays_d": self.rays_d_video[i]}
-            for i in range(num_frames_for_video)
-        ]
+        if use_test_split_for_video:
+            rays_o, rays_d, pixels, poses = self.read_data("test", self.directions)
+            num_frames_for_video = rays_o.shape[0]
+            self._video_inputs = [
+                {"rays_o": rays_o[i], "rays_d": rays_d[i]}
+                for i in range(num_frames_for_video)
+            ]
+            self._video_outputs = [pixels[i] for i in range(num_frames_for_video)]
+        else:
+            rays_o, rays_d = self.generate_video_rays(self.directions, max_height=0.5)
+            num_frames_for_video = rays_o.shape[0]
+            self._video_inputs = [
+                {"rays_o": rays_o[i], "rays_d": rays_d[i]}
+                for i in range(num_frames_for_video)
+            ]
+            self._video_outputs = None
 
         # Simulate regions
         self._input_regions, self._output_regions = region_simulator.simulate_regions(
@@ -160,12 +191,12 @@ class NeRFSyntheticDataset(Dataset):
         self._input = self._input_regions[region]
         self._output = self._output_regions[region]
 
-    def read_intrinsics(self) -> None:
+    def read_intrinsics(self) -> Tuple[Tensor, Tensor]:
         """Read and store local camera intrinsics as instance variables.
-        Instance variables:
+
+        Returns:
             K: (3, 3, 3) Camera intrinsics matrix.
             directions: (h * w, 3) Directions of rays towards each pixel in camera coordinates.
-            img_wh: 2-tuple of image height and width.
         """
 
         # Meta data `camera_angle_x` is the same across splits, so just read from train
@@ -175,15 +206,22 @@ class NeRFSyntheticDataset(Dataset):
         fx = 0.5 * self.img_w / np.tan(0.5 * meta["camera_angle_x"])
         fy = fx
 
-        self.K = torch.tensor(
+        K = torch.tensor(
             [[fx, 0, self.img_w / 2], [0, fy, self.img_h / 2], [0, 0, 1]],
             dtype=torch.float32,
         )
-        self.directions = get_ray_directions(self.img_h, self.img_w, self.K)
+        directions = get_ray_directions(self.img_h, self.img_w, K)
+        return K, directions
 
-    def read_data(self, split: str) -> None:
+    def read_data(
+        self, split: str, directions: Tensor
+    ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
         """Read and store image frames and their corresponding poses.
-        Instance variables:
+        Args:
+            split: Split to use.
+            directions: (h * w, 3) Directions of rays towards each pixel in camera coordinates.
+
+        Returns:
             rays_o: (num_frames, h * w, 3) Origins of all rays in all image frames in
                 world coordinate.
             rays_d: (num_frames, h * w, 3) Corresponding ray directions.
@@ -193,16 +231,15 @@ class NeRFSyntheticDataset(Dataset):
                 sanity check where we could make the test video poses the same as
                 training poses.
         """
-        self.rays_o = []
-        self.rays_d = []
-        self.pixels = []
-        self.poses = []
+        rays_o = []
+        rays_d = []
+        pixels = []
+        poses = []
 
         with open(os.path.join(self.path, f"transforms_{split}.json"), "r") as f:
             frames = json.load(f)["frames"]
 
         print(f"Loading {len(frames)} {split} images ...")
-        heights = []
         for frame in tqdm(frames):
             c2w = torch.tensor(frame["transform_matrix"], dtype=torch.float32)[:3, :4]
 
@@ -211,37 +248,41 @@ class NeRFSyntheticDataset(Dataset):
 
             # NOTE: leaving this here as `ngp_pl` has it. But having scale be 1 makes
             # the cameras all within the [-1, 1] bound (on x and y; within [0, 1] on z).
-            pose_radius_scale = 1.5
-            # pose_radius_scale = 1
+            # If we have scale 1.5, then z is in [0, 1.5].
+            # pose_radius_scale = 1.5
+            pose_radius_scale = 1
             c2w[:, 3] /= torch.linalg.norm(c2w[:, 3]) / pose_radius_scale
-            self.poses.append(c2w)
+            poses.append(c2w)
 
             # Obtain rays in world coordinate
-            rays_o, rays_d = get_rays(self.directions, c2w)
-            self.rays_o.append(rays_o)
-            self.rays_d.append(rays_d)
+            rays_o_frame, rays_d_frame = get_rays(directions, c2w)
+            rays_o.append(rays_o_frame)
+            rays_d.append(rays_d_frame)
 
             # Record corresponding pixel values to these rays
             img_path = os.path.join(self.path, f"{frame['file_path']}.png")
             img = read_image(img_path, (self.img_w, self.img_h))
-            self.pixels.append(img)
+            pixels.append(img)
 
-            # Record average height of this frame
-            heights.append(rays_o[:, 2].mean())
+        rays_o = torch.stack(rays_o)  # (num_frames, h * w, 3)
+        rays_d = torch.stack(rays_d)  # (num_frames, h * w, 3)
+        pixels = torch.stack(pixels)  # (num_frames, h * w, 3)
+        poses = torch.stack(poses)  # (num_frames, 3, 4)
 
-        self.rays_o = torch.stack(self.rays_o)  # (num_frames, h * w, 3)
-        self.rays_d = torch.stack(self.rays_d)  # (num_frames, h * w, 3)
-        self.pixels = torch.stack(self.pixels)  # (num_frames, h * w, 3)
-        self.poses = torch.stack(self.poses)  # (num_frames, 3, 4)
-        self.mean_h = sum(heights) / len(heights)
+        return rays_o, rays_d, pixels, poses
 
-    def generate_video_rays(self) -> None:
-        """Generate rays to visualize a test time video."""
-        #### TEMP: Use train poses as video poses ####
-        self.rays_o_video = self.rays_o
-        self.rays_d_video = self.rays_d
-        return
+    def generate_video_rays(
+        self, directions: Tensor, max_height: float
+    ) -> Tuple[Tensor, Tensor]:
+        """Generate novel rays to visualize a test time video. We do not have ground-truth
+        pixels corresponding to these rays. Rays have 3 heights evenly spaced in [0, max_height].
+        Args:
+            directions: (h * w, 3) Directions of rays towards each pixel in camera coordinates.
 
+        Returns:
+            rays_o_video: (num_frames, h * w, 3)
+            rays_d_video: (num_frames, h * w, 3)
+        """
         # Evaluate on 3 different heights
         c2ws = []
         for i in range(3):
@@ -251,7 +292,7 @@ class NeRFSyntheticDataset(Dataset):
             # height + pitch angle
             pose = torch.FloatTensor(
                 create_spheric_poses(
-                    radius=1, mean_h=self.mean_h / 5 * ((i + 1) / 3), n_poses=40
+                    radius=1, mean_h=max_height * ((i + 1) / 3), n_poses=10
                 )
             )
             pose[:, :, 3] /= torch.linalg.norm(pose[:, :, 3], axis=1, keepdim=True)
@@ -259,16 +300,15 @@ class NeRFSyntheticDataset(Dataset):
 
         c2ws = torch.concatenate(c2ws)
 
-        self.rays_o_video = []
-        self.rays_d_video = []
+        rays_o_video = []
+        rays_d_video = []
 
         for c2w in c2ws:
-            rays_o, rays_d = get_rays(self.directions, c2w)
-            self.rays_o_video.append(rays_o)
-            self.rays_d_video.append(rays_d)
+            rays_o, rays_d = get_rays(directions, c2w)
+            rays_o_video.append(rays_o)
+            rays_d_video.append(rays_d)
 
-        self.rays_o_video = torch.stack(self.rays_o_video)
-        self.rays_d_video = torch.stack(self.rays_d_video)
+        return torch.stack(rays_o_video), torch.stack(rays_d_video)
 
     def __len__(self) -> int:
         """Dataset size is defined as the total number pixels / rays."""
@@ -329,3 +369,8 @@ class NeRFSyntheticDataset(Dataset):
     @property
     def video_inputs(self) -> List[Dict[str, Tensor]]:
         return self._video_inputs
+
+    @property
+    def video_outputs(self) -> Optional[List[Tensor]]:
+        """If None, it means that we do not have ground-truth pixels for video rays."""
+        return self._video_outputs
